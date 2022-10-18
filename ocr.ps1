969bd936b700e4d8d4a2757b1ef20aa9c5972aa2
Sherry <sheryang@microsoft.com>
2022-10-17 18:57:44 -0700

    Fix Read API call.

--- ocr.ps1
+++ ocr.ps1
@@ -17,15 +17,37 @@ $headers.Add( "Content-Type","application/json" )
 
 $body = "{'url' : '$img'}"
 
-write-host "Analyzing image...`n"
-$result = Invoke-RestMethod -Method Post `
+write-host "Analyzing Image...`n"
+$result = Invoke-Webrequest -Method Post `
           -Uri "$endpoint/vision/v3.2/read/analyze?language=en" `
           -Headers $headers `
-          -Body $body | ConvertTo-Json -Depth 6
+          -Body $body 
 
-$analysis = ($result | ConvertFrom-Json)
+# Extract the URL from the response of the Read operation
+# to call the API to getting the analysis results 
+$resultUrl = $($result.Headers['Operation-Location'])
 
-foreach ($listofdict in $analyzeResult.readResults.lines.words)
+# Create the header for the REST GET with only the subscription key
+$resultHeaders = @{}
+$resultHeaders.Add( "Ocp-Apim-Subscription-Key", $key )
+$body = "{'url' : '$img'}"
+
+# Get the read results, passing in the resultURL
+# Continue to request results until the analysis is "succeeded"
+Write-Host "Getting results..."
+Do {
+    $result = Invoke-RestMethod -Method Get `
+            -Uri $resultUrl `
+            -Headers $resultHeaders | ConvertTo-Json -Depth 10
+
+    $analysis = ($result | ConvertFrom-Json)
+} while ($analysis.status -ne "succeeded")
+
+# Access the relevant fields from the analysis 
+$analysisFields = $analysis.analyzeResult.readResults.lines
+
+# Print out the text and bounding boxes 
+foreach ($listofdict in $analysisFields)
 {
     foreach($dict in $listofdict)
     {
